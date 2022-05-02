c46d26c2457a4a10f4265c40601ea8c0e5010764
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-05-02 16:53:53 +0200

    Structure setup

---
+++ src/services/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { User } from "@/types/types";
+import { LoginUser, RegisterUser } from "@/types/api";
+
+const client = axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+client.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+export default {
+  registerUser: async (user: RegisterUser): Promise<User> => {
+    return await client.post("/users/register", user)
+  },
+  loginUser: async (user: LoginUser): Promise<User> => {
+    return await client.post("/users/login", user)
+  },
+};
